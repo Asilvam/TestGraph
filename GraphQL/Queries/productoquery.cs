7494473f60b54d1d22dac24cf244ecc7d8ded943
alejandro silva <alejandro.silva.m@gmail.com>
2020-07-05 22:33:31 -0400

    desarrollo

--- GraphQL/Queries/productoquery.cs
+++ GraphQL/Queries/productoquery.cs
@@ -1,19 +1,40 @@
 ﻿using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestGraph.GraphQL.Types;
 using TestGraph.Models;
 
 namespace TestGraph.GraphQL.Queries
 {
     public class productoquery: ObjectGraphType
     {
-        private readonly modelContext context;
+        private readonly modelContext _context;
         public productoquery(modelContext context)
         {
-            this.context = context;
-
+            this._context = context;
+            Field<ListGraphType<productotype>>(
+                "Productos",
+                arguments: new QueryArguments(new List<QueryArgument>
+                {
+                    new QueryArgument<IdGraphType>
+                    {
+                        Name="id"
+                    }
+                }),
+                resolve: context =>
+                {
+                    var ProductId = context.GetArgument<int?>("id");
+                    if (ProductId.HasValue)
+                    {
+                        var res = _context.Products.Where(l => l.ProductId == ProductId).ToListAsync();
+                        return res;
+                    }
+                    return _context.Products.ToListAsync();
+                }
+                );
         }
     }
 }
---
+++ GraphQL/SchemaGraph/TestSchema.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestGraph.GraphQL.Queries;
+
+namespace TestGraph.GraphQL.SchemaGraph
+{
+    public class TestSchema:Schema
+    {
+        public TestSchema(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<productoquery>();
+        }
+    }
+}
